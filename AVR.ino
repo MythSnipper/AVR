43090c1010afa320db2f411b8315bc219dfe3334
MythSnipper <mythsnipper2022@gmail.com>
2025-10-21 08:37:49 +0300

    jogisdfr

--- AVR.ino
+++ AVR.ino
@@ -2,14 +2,25 @@
 
 //the screen
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+//wiring
+/*
+VSS GND
+VDD 5V
+VO potentiometer middle
+RS 12
+RW GND
+E 11
+D4-7 5-2
+A(LED+) 5V+330Ω resistor
+K(LED-) GND
+*/
 
 void setup(){
 	pinMode(13, OUTPUT);
     Serial.begin(115200);
-
     lcd.begin(16, 2);
     lcd.print("Hello, World!");
+    lcd.blink()
 }
 
 int sleepTime = 500;
