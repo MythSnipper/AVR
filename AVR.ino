3b5cdf7575985e460b309034836a4dddde7311e5
MythSnipper <mythsnipper2022@gmail.com>
2025-10-21 18:18:20 +0300

    ffhpifphisdf

--- AVR.ino
+++ AVR.ino
@@ -15,19 +15,25 @@ A(LED+) 5V+330Ω resistor
 K(LED-) GND
 
 button 2
+piezo 3
 */
 
 const uint8_t buttonPin = 2;
+const uint8_t piezoPin = 3;
 const uint8_t ledPin = 13;
 
 
 
-bool lastState = LOW;
+bool lastState = !LOW;
 uint32_t lastTime;
-const uint8_t wpm = 20;
+
+const uint32_t piezo_frequency = 220;
+
+const uint8_t wpm = 10;
+
 uint32_t dot_len;
 uint32_t dash_len;
-uint32_t intra_char_len;
+uint32_t dot_thres; //threshold, if ms below this it is considered to be a dot, otherwise dash
 uint32_t inter_char_len;
 uint32_t inter_word_len;
 
@@ -35,40 +41,78 @@ void setup(){
     //morse code input and output
     pinMode(buttonPin, INPUT_PULLUP);
     pinMode(ledPin, OUTPUT);
+    pinMode(piezoPin, OUTPUT);
 
     Serial.begin(115200);
     lcd.begin(16, 2);
     lcd.blink();
 
     //calculate some things
-    dot_len = 120 / wpm;
+    dot_len = 1200 / wpm;
     dash_len = 3 * dot_len;
-    intra_char_len = dot_len;
+    dot_thres = 1.5 * dot_len;
     inter_char_len = 3 * dot_len;
     inter_word_len = 7 * dot_len;
 
-    delay(2000);
 
     Serial.println("Hello World!");
-    Serial.println("Dot length: "+dot_len);
-    Serial.println("Dash length: "+dash_len);
-    Serial.println("intra char length: "+intra_char_len);
-    Serial.println("inter char length: "+inter_char_len);
-    Serial.println("inter word length: "+inter_word_len);
-    Serial.println("Waiting for first button press...");
-    while(!digitalRead(buttonPin));
+    Serial.print("Dot length: ");Serial.println(dot_len);
+    Serial.print("Dash length: ");Serial.println(dash_len);
+    Serial.print("inter char length: ");Serial.println(inter_char_len);
+    Serial.print("inter word length: ");Serial.println(inter_word_len);
+    
+    tone(piezoPin, piezo_frequency);
+    delay(30);
+    noTone(piezoPin);
+    delay(100);
+    tone(piezoPin, piezo_frequency);
+    delay(30);
+    noTone(piezoPin);
+    Serial.println("Start");
+
+    lastTime = millis();
 }
 
 void loop(){
-    bool state = digitalRead(buttonPin);
-	
-    lcd.print("O");
-    delay(400);
-    lcd.print("A");
-    delay(400);
-    lcd.print("H");
-    delay(400);
-    
+    bool state = !digitalRead(buttonPin);
+	digitalWrite(ledPin, state);
+
+    if(state){
+        tone(piezoPin, piezo_frequency);
+    }
+    else{
+        noTone(piezoPin);
+    }
+    if(state != lastState){
+        uint32_t dt = millis() - lastTime;
+        lastTime = millis();
+        //Serial.print(((lastState)?"HIGH":"LOW"));Serial.print(" ");Serial.println(dt, DEC);Serial.print("ms ");
+        if(lastState){ //time segment was button on high
+            if(dt > 10){
+                if(dt < dot_thres){
+                    Serial.print(".");
+                }
+                else{
+                    Serial.print("-");
+                }
+            }
+            else{
+                Serial.println("ignored");
+            }
+        }
+        else{
+            if(dt > inter_word_len){
+                Serial.print("/");
+            }
+            else if(dt > inter_char_len){
+                Serial.print(" ");
+            }
+        }
+    }
+
+
+
+    lastState = state;
 }
 
 
